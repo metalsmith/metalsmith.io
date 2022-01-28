62cfd4c390c804262154b10a92f3cf808a425214
Kevin Van Lierde <kevin.van.lierde@gmail.com>
2022-01-28 01:50:06 +0100

    test: swap jest for mocha, enhance tests

--- metalsmith.test.js
+++ metalsmith.test.js
@@ -1,45 +1,49 @@
-/* eslint-env jest */
-
+const assert = require('assert');
+const { describe, it } = require('mocha');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
+const plugins = require('./lib/data/plugins.json');
 
 const sitePath = path.join(__dirname, 'build');
 
-test('build should have key files', () => {
-  const sitePaths = [
-    path.join(sitePath, 'index.html'),
-    path.join(sitePath, 'plugins', 'index.html'),
-    path.join(sitePath, 'api', 'index.html'),
-    path.join(sitePath, 'news', 'index.html'),
-    path.join(sitePath, 'about', 'index.html'),
-    path.join(sitePath, 'index.css'),
-    path.join(sitePath, 'index.js')
-  ];
+describe('metalsmith.io', () => {
+  it('build should have key files', () => {
+    const sitePaths = [
+      path.join(sitePath, 'index.html'),
+      path.join(sitePath, 'plugins', 'index.html'),
+      path.join(sitePath, 'api', 'index.html'),
+      path.join(sitePath, 'step-by-step', 'index.html'),
+      path.join(sitePath, 'news', 'index.html'),
+      path.join(sitePath, 'about', 'index.html'),
+      path.join(sitePath, 'index.css'),
+      path.join(sitePath, 'index.js')
+    ];
 
-  // Divider images
-  for (let i = 1; i < 7; i += 1) {
-    sitePaths.push(path.join(sitePath, '/img', `divider-${i}.png`));
-  }
+    // Divider images
+    for (let i = 1; i < 7; i += 1) {
+      sitePaths.push(path.join(sitePath, '/img', `divider-${i}.png`));
+    }
 
-  sitePaths.forEach(filePath => {
-    expect(() => {
-      fs.accessSync(filePath);
-    }).not.toThrow();
+    sitePaths.forEach(filePath => {
+      assert.doesNotThrow(() => {
+        fs.accessSync(filePath);
+      });
+    });
   });
-});
 
-test('index.html should have key elements', () => {
-  const indexContent = fs.readFileSync(path.join(sitePath, 'index.html')).toString();
-  const $ = cheerio.load(indexContent);
+  it('index.html should have key elements', () => {
+    const indexContent = fs.readFileSync(path.join(sitePath, 'index.html')).toString();
+    const $ = cheerio.load(indexContent);
 
-  expect($('title')[0].children[0].data).toEqual('Metalsmith | Home');
-});
+    assert.strictEqual($('title')[0].children[0].data, 'Metalsmith | Home');
+  });
 
-test('plugins/index.html should have key elements', () => {
-  const indexContent = fs.readFileSync(path.join(sitePath, '/plugins/index.html')).toString();
-  const $ = cheerio.load(indexContent);
+  it('plugins/index.html should have key elements', () => {
+    const indexContent = fs.readFileSync(path.join(sitePath, '/plugins/index.html')).toString();
+    const $ = cheerio.load(indexContent);
 
-  expect($('title')[0].children[0].data).toEqual('Metalsmith | Plugins');
-  expect($('.PluginList li').length).toBeGreaterThan(0);
+    assert.strictEqual($('title')[0].children[0].data, 'Metalsmith | Plugins');
+    assert.strictEqual($('.PluginList li').length, plugins.length);
+  });
 });
